698998bf9dfd44ba30c41bcd75b3414f1c378319
Rahman2001 <rahmanrejepov777@gmail.com>
2024-04-27 16:27:31 +0300

    data cleaning function is created

---
+++ clean_data.py
@@ -0,0 +1,35 @@
+import pandas as pd
+
+# Load data
+df1 = pd.read_csv("datasets/train.csv")
+df2 = pd.read_csv("datasets/test.csv")
+#
+# print("Train set:\n")
+# print(len(df1))
+# print(df1.isnull().sum())
+# print("\n")
+# print("Test set:\n")
+# print(len(df2))
+# print(df2.isnull().sum())
+# print("\n\n")
+
+df1 = df1.dropna()
+df2 = df2.dropna()
+
+# print("Train set (no nulls):\n")
+# print(len(df1))
+# print(df1.isnull().sum())
+# print("\n")
+# print("Test set (no nulls):\n")
+# print(len(df2))
+# print(df2.isnull().sum())
+# print("\n\n")
+print("Total dataset: " + str(len(df1) + len(df2)))
+
+#return cleaned data of test set
+def clean_data_test():
+    return df1
+
+#return cleaned data of train set
+def clean_data_train():
+    return df2
