40175978d126808b3d49e67260be83311dfa8cb8
Rahman2001 <rahmanrejepov777@gmail.com>
2024-05-08 00:18:31 +0300

    forward_selection.py updated

--- random_forest.py
+++ random_forest.py
@@ -6,7 +6,7 @@ from airline_services import services
 
 features = services
 x_train, y_train, x_test, y_test = split_to_train_test(clean_data_train(), clean_data_test(), features, 'satisfaction')
-random_forest = forward_feature_selection(RandomForestClassifier(n_estimators=15))
+random_forest = forward_feature_selection(RandomForestClassifier(n_estimators=10))
 random_forest = random_forest.fit(x_train, y_train)
 
 print(random_forest.get_feature_names_out())
