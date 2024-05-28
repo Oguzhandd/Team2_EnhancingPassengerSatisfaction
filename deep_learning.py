f90947bf88a4580c6adcd26b7c648ed672c135e9
Rahman2001 <rahmanrejepov777@gmail.com>
2024-05-28 22:02:30 +0300

    deep learning model's errors are corrected

--- deep_learning.py
+++ deep_learning.py
@@ -1,61 +1,32 @@
-import time
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.optimizers import Adam
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import classification_report
+from keras import Sequential
+from keras.src.layers import Dense
+from keras.src.optimizers import Adam
 from forward_selection import forward_feature_selection
 from clean_data import clean_data_train, clean_data_test
 from preprocessing_data import split_to_train_test
 from airline_services import services
-from random_forest import x_train
 
 
-def deep_learning(X_train, y_train, X_test, y_test, features):
+def deep_learning():
     model = Sequential()
-    model.add(Dense(64, input_shape=(len(features),), activation='relu'))
+    model.add(Dense(64, activation='relu'))
     model.add(Dense(32, activation='relu'))
     model.add(Dense(1, activation='sigmoid'))
-
-    # Modeli derleme süresini ölç
-    start_time = time.time()
-    model.compile(optimizer=Adam(), loss='binary_crossentropy', metrics=['accuracy'])
-    compile_time = time.time() - start_time
-
-    print(f"Model compile süresi: {compile_time:.2f} saniye")
-
-    model.fit(X_train, y_train, epochs=10, batch_size=64, verbose=1)
-
-    train_loss, train_accuracy = model.evaluate(X_train, y_train, verbose=0)
-    test_loss, test_accuracy = model.evaluate(X_test, y_test, verbose=0)
-
-    print("Train Accuracy:", train_accuracy)
-    print("Test Accuracy:", test_accuracy)
-
-    y_pred_train = (model.predict(X_train) > 0.5).astype("int32")
-    y_pred_test = (model.predict(X_test) > 0.5).astype("int32")
-
-    print("Classification Report for Test Set:")
-    print(classification_report(y_test, y_pred_test))
+    model.compile(optimizer=Adam(), loss='binary_crossentropy')
+    return model
 
 
-if __name__ == "__main__":
-    df_train_cleaned = clean_data_train()
-    df_test_cleaned = clean_data_test()
+df_train_cleaned = clean_data_train()
+df_test_cleaned = clean_data_test()
 
-    X_train, y_train, X_test, y_test = split_to_train_test(df_train_cleaned, df_test_cleaned, services, 'satisfaction')
-    #forward selection.
-    forward_selector = forward_feature_selection(classifier=RandomForestClassifier())
-    forward_selector = forward_selector.fit(X_train, y_train)
+X_train, y_train, X_test, y_test = split_to_train_test(df_train_cleaned, df_test_cleaned, services, 'satisfaction')
 
-    selected_features_indices = forward_selector.get_support(indices=True)
-    selected_features = X_train.columns[selected_features_indices].tolist()
+deep = forward_feature_selection(deep_learning())
+deep = deep.fit(X_train, y_train)
+selected_features = deep.get_feature_names_out()
+print(selected_features)
 
-    X_train_selected = X_train[selected_features]
-    X_test_selected = X_test[selected_features]
-
-    deep_learning(X_train_selected, y_train, X_test_selected, y_test, selected_features)
-
-    deep_learning = RandomForestClassifier(n_estimators=10)
-    deep_learning.fit(x_train, y_train)
-    y_pred = deep_learning.predict(x_train)
+deep = deep_learning()
+X_train, y_train, X_test, y_test = split_to_train_test(df_train_cleaned, df_test_cleaned, selected_features, 'satisfaction')
+deep.fit(X_train, y_train)
+y_train_pred = deep.predict(X_train)
---
+++ rule_induction_temp.py
@@ -0,0 +1,21 @@
+
+from clean_data import clean_data_train, clean_data_test
+from preprocessing_data import split_to_train_test
+from airline_services import services
+from forward_selection import forward_feature_selection
+
+df_train_cleaned = clean_data_train()
+df_test_cleaned = clean_data_test()
+
+X_train, y_train, X_test, y_test = split_to_train_test(df_train_cleaned, df_test_cleaned, services, 'satisfaction')
+
+rule = forward_feature_selection()
+rule = rule.fit(X_train, y_train)
+selected_features = rule.get_feature_names_out()
+print(selected_features)
+
+x_train, y_train, x_test, y_test = split_to_train_test(df_train_cleaned,
+                                                       df_test_cleaned, selected_features, 'satisfaction')
+supvm = svm.SVC()
+supvm.fit(x_train, y_train)
+y_pred = supvm.predict(x_train)
